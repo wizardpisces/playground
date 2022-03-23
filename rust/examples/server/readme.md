f55259f151740d3e7593ad6038ac7cdf2720590b
刘泽 <1275839779@qq.com>
2022-03-23 15:42:27 +0800

    chore:modify rust server

--- rust/examples/server/readme.md
+++ rust/examples/server/readme.md
@@ -1,10 +1,15 @@
-Question
+## How to run
 
-- wrap/unwrap做了啥？
+```bash
+cargo run
+```
+
+## Question
+
 - mpsc::channel
-- use std::io::prelude::*; 为什么能给 stream 自动 增加 read trait
+- use std::io::prelude::\*; 为什么能给 stream 自动 增加 read trait
 - 线程安全智能指针
-- Box
+- Box 有点类似 js 函数蹦床，做一个阻断
 - JoinHandle
 - 使用带有泛型和 Fn trait 的闭包
 - Arc<Mutex<mpsc::Receiver<Job>>>
--- rust/examples/server/src/lib.rs
+++ rust/examples/server/src/lib.rs
@@ -1,30 +1,20 @@
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
-
-use std::thread;
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::sync::mpsc;
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
 
 impl ThreadPool {
-    // pub fn new(size: usize) -> ThreadPool {
-    //     assert!(size > 0);
-
-    //     let (sender, receiver) = mpsc::channel();
-
-    //     let mut workers = Vec::with_capacity(size);
-
-    //     for id in 0..size {
-    //         workers.push(Worker::new(id, receiver));
-    //     }
-
-    //     ThreadPool { workers, sender }
-    // }
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
@@ -47,26 +37,54 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
-    
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+         println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
 
-            println!("Worker {} got a job; executing.", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
 
-            job();
+                    break;
+                }
+            }
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
