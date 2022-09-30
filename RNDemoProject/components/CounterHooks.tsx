34cc31579d35a24a1a44b7258d25b579dc999d12
wizardpisces <1275839779@qq.com>
2022-09-30 14:52:31 +0800

    chore: test rn networking

--- RNDemoProject/components/CounterHooks.tsx
+++ RNDemoProject/components/CounterHooks.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 import {
     TouchableOpacity,
@@ -35,13 +35,42 @@ const styles = StyleSheet.create({
         color: '#000'
     }
 })
+type Movie = {id:number,title:string,releaseYear:string}
+const getMoviesFromApi = (): Promise<Movie[]> => {
+    return fetch('https://reactnative.dev/movies.json')
+        .then((response) => response.json())
+        .then((json) => {
+            return json.movies;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 
+};
 export default function CounterHooks() {
     let [count, setCount] = useState(1)
+    let [movies, setMovies] = useState<Movie[]>([])
+    const getMovies = ()=>{
+        getMoviesFromApi().then(moviesResult=>{
+            debugger
+
+            console.log('moviesResult',moviesResult)
+            setMovies(moviesResult)
+        })
+    }
+    useEffect(() => {
+    })
     return (<Fragment>
         <Text style={count < 3 ? styles.less : styles.greater}>Counter-Hooks : You clicked {count} times</Text>
-        <TouchableOpacity style={styles.button} onPress={()=>setCount(count+1)}>
+        <TouchableOpacity style={styles.button} onPress={() => setCount(count + 1)}>
             <Text style={styles.buttonText}>Click</Text>
         </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.button} onPress={() => getMovies()}>
+            <Text style={styles.buttonText}>getMovies</Text>
+        </TouchableOpacity>
+        {movies.map(movie=><Text key={movie.id}>{movie.title}</Text>)}
+
+
     </Fragment>)
 }
