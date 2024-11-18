f2d5729830600364bc123e12f8024e480f5fe12c
wizardpisces <1275839779@qq.com>
2024-11-18 16:30:44 +0800

    test: RNDemoProject e2e 录制与回放

--- RNDemoProject/e2e/appium.js
+++ RNDemoProject/e2e/appium.js
@@ -15,12 +15,52 @@ const opts = {
 };
 
 async function main() {
-  const client = await remote(opts);
-  const element = await client.$("~counter-button");
+  const driver = await remote(opts);
+  const element = await driver.$("~counter-button");
   await element.click();
   await element.click();
   await element.click();
-  await client.deleteSession();
+
+  await driver.action('pointer')
+    .move({ duration: 0, x: 198, y: 343 })
+    .down({ button: 0 })
+    .pause(50)
+    .up({ button: 0 })
+    .perform();
+
+  const el1 = await driver.$("accessibility id:counter-button");
+  await el1.click();
+  await el1.click();
+  const el2 = await driver.$("accessibility id:Click");
+  await el2.click();
+  await driver.action('pointer')
+    .move({ duration: 0, x: 237, y: 239 })
+    .down({ button: 0 })
+    .pause(50)
+    .up({ button: 0 })
+    .perform();
+
+  const el3 = await driver.$("accessibility id:Tab Two, tab, 2 of 2");
+  await el3.click();
+  const el4 = await driver.$("accessibility id:Tab One, tab, 1 of 2");
+  await el4.click();
+
+  await driver.action('pointer')
+    .move({ duration: 0, x: 370, y: 78 })
+    .down({ button: 0 })
+    .pause(50)
+    .up({ button: 0 })
+    .perform();
+
+  await driver.action('pointer')
+    .move({ duration: 0, x: 195, y: 88 })
+    .down({ button: 0 })
+    .move({ duration: 1000, x: 202, y: 736 })
+    .up({ button: 0 })
+    .perform();
+
+
+  await driver.deleteSession();
 }
 
 main();
